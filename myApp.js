2e7ddccd93cfe9ea8dcba96c48542154827e3bb6
felipe <felipebomfim4161@gmail.com>
2022-11-20 15:42:02 -0300

    Finished basic Express and Node App

--- myApp.js
+++ myApp.js
@@ -1,34 +1,50 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+console.log("Hello World");
+
+
+app.use("/",function (req,res,next){
+  console.log(req.method+" "+req.path+" - "+req.ip);
+  next();
+});
+
+app.use("/public", express.static(__dirname+"/public"));
+
+app.get('/',function (req,res){
+  res.sendFile(__dirname+"/views/index.html");
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/json',function (req,res){
+  message = "Hello json"
+  if (process.env['MESSAGE_STYLE'] === "uppercase")
+      message = message.toUpperCase();
+  res.json({"message": message});
+});
+
+app.get('/now',function (req,res,next){
+  req.time = new Date().toString();
+  next();
+}, function (req,res){
+  res.json({"time": req.time});
+});
+
+app.get('/:word/echo',function (req,res){
+  res.json({"echo": req.params.word});
+});
+
+app.route('/name').get(function (req,res){
+  res.json({"name": req.query['first']+" "+req.query['last']});
+}).post(function(req,res){
+  res.json({"name": req.body['first']+" "+req.body['last']});
+});
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// app.use(bodyParser.json());
+// const mySecret = process.env['MESSAGE_STYLE']
 
 
 
